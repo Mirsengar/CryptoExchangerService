c9554dfadfe8b406f0669f87000aa1d1a747d59b
Mirsen Gar <92634638+Mirsengar@users.noreply.github.com>
2022-12-08 20:30:05 +0330

    Add files via upload

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"CryptoExchangerServer/client"
+	"CryptoExchangerServer/mm"
+	"CryptoExchangerServer/server"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	go server.StartServer()
+	time.Sleep(1 * time.Second)
+
+	c := client.NewClient()
+
+	cfg := mm.Config{
+		UserID:         8,
+		OrderSize:      10,
+		MinSpread:      20,
+		MakeInterval:   1 * time.Second,
+		SeedOffset:     40,
+		ExchangeClient: c,
+		PriceOffset:    10,
+	}
+	maker := mm.NewMakerMaker(cfg)
+
+	maker.Start()
+
+	time.Sleep(2 * time.Second)
+	go marketOrderPlacer(c)
+
+	select {}
+}
+
+func marketOrderPlacer(c *client.Client) {
+	ticker := time.NewTicker(500 * time.Millisecond)
+
+	for {
+		randint := rand.Intn(10)
+		bid := true
+		if randint < 7 {
+			bid = false
+		}
+
+		order := client.PlaceOrderParams{
+			UserID: 7,
+			Bid:    bid,
+			Size:   1,
+		}
+
+		_, err := c.PlaceMarketOrder(&order)
+		if err != nil {
+			panic(err)
+		}
+
+		<-ticker.C
+	}
+}
